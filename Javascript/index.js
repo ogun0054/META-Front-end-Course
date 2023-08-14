2df63b56a6d8ffe2d8597a03d319c473ada02e71
github_i-slot <ismailadesholaharun@gmail.com>
2023-08-14 14:03:03 +0100

    Javascript part of the Meta Front-End Course

---
+++ Javascript/index.js
@@ -0,0 +1,54 @@
+/*Operators 
+  Operators:  An operator is used to manipulate individual data items and return a result.
+  e.g Assignment operator (=)
+  Arithmetic Operators
+  - Addition (+)
+  - Subtraction (-)
+  - Division (/)
+  - Multiplication (*)
+  - 
+*/
+
+// Assignment Operation
+let person = "AbduLlah";
+
+// Addition Operation
+let addition = 2 + 7;
+
+// Subtraction Operation
+let sub = 10 - 7;
+
+// Division Operation
+let div = 100 / 20;
+
+// Multiplication Operation
+let multiple = 16 * 4;
+
+console.log(" Arithmetic Operations " + addition, sub, div, multiple);
+
+/*
+ Comparison Operators
+
+ - Greater than ( > )
+ - Less than ( < )
+ - Equal to ( == )
+ - Not equal to ( != )
+*/
+
+let greater = 10 > 5;
+let less = 5 < 10;
+let equal = 50 == 50;
+let notEqual = 50 != 60;
+
+console.log("Comparison Operators   " + greater, less, equal, notEqual);
+
+/*
+Logical Operators
+Logical Operators : Boolean ( True / False)
+
+- And (&&) : Checks for both conditions to be true, e.g (a>5 && a<10);
+- OR (||) : Checks for at least one conditions to be true e.g (a>5 || a >10)
+- NOT (!) : Returns false if the result is true e.g !( a>5 )
+
+** Operators are used to control the flow of a program that meets certain criteria.
+*/
