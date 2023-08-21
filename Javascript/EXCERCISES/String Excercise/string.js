8e88ba531d0c22022e9d3ca71dce427d0c4cd6cd
github_i-slot <ismailadesholaharun@gmail.com>
2023-08-21 12:58:56 +0100

    object and Array Exercise

---
+++ Javascript/EXCERCISES/String Excercise/string.js
@@ -0,0 +1,47 @@
+/*
+Exercise: Creating arrays and objects
+In this exercise lab you will practice creating arrays and objects.
+
+Tasks to complete
+Create a new empty array literal and assign it to the variable clothes.
+
+Add 5 of your favorite items of clothing as strings using the push() method.
+
+Remove the fifth piece of clothing from the array using the pop() method.
+
+Add a new piece of clothing using the push() method.
+
+Use console.log to show the third item from the clothes array in the console.
+
+Create a new empty object literal and assign it to the variable favCar.
+
+Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+
+Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+
+Use the console to log the entire favCar object.
+ */
+// solution
+
+let clothes = [];
+clothes.push("Jalab");
+clothes.push("Agbada");
+clothes.push("Kaftan");
+clothes.push("Shirt");
+clothes.push("Guinea");
+
+console.log(clothes);
+
+clothes.pop();
+console.log(clothes);
+
+clothes.push("Voil");
+console.log(clothes);
+console.log(clothes[2]);
+
+let favCar = {};
+favCar.color = "white";
+favCar.convertible = true;
+
+console.log(favCar);
+console.log(favCar["color"]);
