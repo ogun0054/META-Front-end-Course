e9d6e8742ea4d5f48725cec75a70fe10296fdeb4
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-01-25 20:00:30 -0500

    update

---
+++ React/customizing-template/src/components/Footer.js
@@ -0,0 +1,10 @@
+// import React from "react"
+export default function Footer() {
+    return (
+        <div className="copyright">
+            <p>Made with love by Myself</p>
+        </div>
+    );
+};
+
+// export default Footer;
