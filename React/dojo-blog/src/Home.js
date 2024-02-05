83fb82604a121b9d49bb634354e33d2cbc40fc3f
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-02-05 00:36:01 -0500

    click event

--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("hello, Ninjas", e)  
+      } 
+
+    const handleClickAgain = (name) => {
+        console.log(`Hello ${name} `)
+    }
     return ( 
         <div>
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() =>
+                handleClickAgain("Ismail")
+            }>Click me</button>
         </div>
      );
 }
