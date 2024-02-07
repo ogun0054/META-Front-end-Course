3070e9c75f5acad3580e8677a52d1afb2baa0fb9
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-02-06 23:05:10 -0500

    update

--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -1,19 +1,30 @@
+import { useState } from "react"
+
 const Home = () => {
+    // let name = "Ismail"
+    const [name, setName] = useState("Ismail");
+    const [age, setAge] = useState(0)
 
     const handleClick = (e) => {
-        console.log("hello, Ninjas", e)  
+        // console.log("hello, Ninjas")  
+        setName("Aisha");
+        setAge(age + 1);
+        
       } 
 
-    const handleClickAgain = (name) => {
-        console.log(`Hello ${name} `)
-    }
+    // const handleClickAgain = (name) => {
+    //     console.log(`Hello ${name} `)
+    // }
+  
     return ( 
         <div>
             <h2>Home Page</h2>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() =>
+            {/* <button onClick={() =>
                 handleClickAgain("Ismail")
-            }>Click me</button>
+            }>Click me</button> */}
+
+            <p>{name} will be {age} by November 17 {2023 + 1}</p>
         </div>
      );
 }
